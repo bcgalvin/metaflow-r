81332fd2ba7765163503a5c0879625979523e146
Bryan Galvin <bcgalvin@gmail.com>
2024-10-03 17:01:25 -0700

    adds package.R and update zzz.R to use custom install function

---
+++ R/package.R
@@ -0,0 +1,45 @@
+#' Metaflow for R
+#'
+#' @description
+#' \href{https://metaflow.org/}{Metaflow} is an open-source framework
+#' for data science projects. It's designed to handle the entire lifecycle
+#' of data science workflows, from prototyping to production deployment.
+#' Metaflow allows you to write and manage data processing pipelines using
+#' a simple, intuitive API.
+#'
+#' @details
+#' The \href{https://docs.metaflow.org/}{Metaflow API} is primarily
+#' implemented in Python, with bindings for R. This package provides access
+#' to the complete Metaflow functionality from within R.
+#'
+#' Key features of Metaflow include:
+#' \itemize{
+#'   \item Seamless scaling from prototype to production
+#'   \item Built-in versioning of data and code
+#'   \item Easy integration with cloud computing resources
+#'   \item Support for parallel and distributed computing
+#' }
+#'
+#' @section Documentation:
+#' For additional documentation on the Metaflow package, see
+#' \href{https://docs.metaflow.org/v/r/}{https://docs.metaflow.org/v/r/}
+#'
+#' @section Enterprise Support:
+#' For more information about Metaflow and its enterprise offerings, visit
+#' \href{https://outerbounds.com/}{Outerbounds}
+#'
+#'
+#' @docType package
+#' @aliases metaflow-package
+#' @name metaflow
+#' @seealso
+#' \code{\link{install_metaflow}} for installing Metaflow and its dependencies
+#' @author Bryan Galvin (bcgalvin@@gmail.com)
+#' @keywords package
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom lifecycle deprecated
+#' @importFrom testthat test_that expect_identical expect_error expect_silent
+#' @importFrom testthat local_mocked_bindings
+## usethis namespace: end
--- R/zzz.R
+++ R/zzz.R
@@ -1,15 +1,50 @@
+# Combine globals
+.globals <- new.env(parent = emptyenv())
+mf <- NULL
+
+#' Handle to the `metaflow` module
+#'
+#' @export
+#' @return Module(metaflow)
+mf <- NULL
+
 .onLoad <- function(libname, pkgname) {
-  reticulate::configure_environment(pkgname)
-  .check_python_installed()
+  .import_metaflow()
 }
 
-#' @import cli
-.check_python_installed <- function() {
-  a <- reticulate::py_discover_config()
-  if (is.null(a) || length(a)<1) {
-    cli::cli_alert_warning('Could not locate Python installation, which will limit ability to query from model results.')
-    cli::cli_inform('Try `reticulate::install_miniconda()`, or see https://rstudio.github.io/reticulate/reference/install_miniconda.html')
-  } else {
-    cli::cli_alert_success(glue::glue('Python installation found (Default: {a$python}).'))
+.onAttach <- function(libname, pkgname) {
+  tryCatch({
+    if (is_metaflow_available()) {
+      packageStartupMessage(
+        "Metaflow Python module successfully loaded.\n",
+        "Metaflow version: ", .globals[["mf"]][["__version__"]], "\n",
+        "Python executable: ", reticulate::py_config()[["python"]]
+      )
+    } else {
+      packageStartupMessage(
+        "Metaflow Python module not loaded. ",
+        "Use install_metaflow() to install it."
+      )
+    }
+  }, error = function(e) {
+    packageStartupMessage("Error during package attachment: ", e[["message"]])
+  })
+}
+
+.import_metaflow <- function() {
+  mf <<- reticulate::import(
+    "metaflow",
+    delay_load = list(
+      priority = 5L,
+      on_load = function() {},
+      on_error = function(e) NULL
+    )
+  )
+  if (!is.null(mf)) {
+    assign("mf", mf, envir = .globals)
   }
+}
+
+is_metaflow_available <- function() {
+  !is.null(.globals[["mf"]]) && !is.null(.globals[["mf"]][["__version__"]])
 }
